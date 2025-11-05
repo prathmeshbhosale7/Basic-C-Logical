862a3d1975a36fabbe96ac341a094b3105c69242
Prathmesh Bhosale <prathmeshm.bhosale@gmail.com>
2025-11-06 00:41:28 +0530

    Add files via upload

---
+++ SwapStringWithoutThirdVariable.cs
@@ -0,0 +1,25 @@
+﻿namespace SwapStringWithoutThirdVariable
+{
+    internal class SwapStringWithoutThirdVariable
+    {
+        static void Main()
+        {
+            string str1, str2;
+
+            Console.Write("Enter str1: ");
+            str1 = Console.ReadLine();
+
+            Console.Write("Enter str2: ");
+            str2 = Console.ReadLine();
+
+            str1 += str2;
+
+            str2 = str1.Substring(0, str1.Length - str2.Length);
+
+            str1 = str1.Substring(str2.Length);
+
+            Console.WriteLine(str1);
+            Console.WriteLine(str2);
+        }
+    }
+}
