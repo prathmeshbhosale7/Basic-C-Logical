5567e1eb095df0a7cdb20bfba30285b44e9087cd
Prathmesh Bhosale <prathmeshm.bhosale@gmail.com>
2026-02-15 00:35:15 +0530

    OOPS Encapsulation Realtime Example

---
+++ OOPS Encapsulation/Account.cs
@@ -0,0 +1,33 @@
+﻿class Account()
+{
+    private int balance = 1000;
+
+    public void Withdraw(int a)
+    {
+        if(a <= balance)
+        {
+            balance -= a;
+        }
+        else
+        {
+            Console.WriteLine("Insufficient Balance");
+        }
+    }
+
+    public void Deposit(int a)
+    {
+        if(a > 0)
+        {
+            balance += a;
+        }
+        else
+        {
+            Console.WriteLine("Invalid Balance");
+        }
+    }
+
+    public void Display()
+    {
+        Console.Write("Available Balance is: " + balance);
+    }
+}
